28577084ce1d17fecbe13c65bcf88b9bfbd30109
admin <admin@DESKTOP-08GTU3I.bbrouter>
2024-02-07 13:57:04 +0530

    I have updated the code.

--- src/test/java/com/login/Login.java
+++ src/test/java/com/login/Login.java
@@ -1,7 +1,15 @@
 package com.login;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Login {
-	
+	public static void main(String[] args) {
+		WebDriver hospiol = new ChromeDriver();
+		hospiol.manage().window().maximize();
+		
+		
+	}
 	
 
 }
