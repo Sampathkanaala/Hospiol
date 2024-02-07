9bbb0a3ecb3c47e18eefaff9f00bf1276680cc22
admin <admin@DESKTOP-08GTU3I.bbrouter>
2024-02-07 14:07:43 +0530

    I have Updated the Project

--- src/test/java/com/login/Login.java
+++ src/test/java/com/login/Login.java
@@ -1,5 +1,6 @@
 package com.login;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,8 +8,10 @@ public class Login {
 	public static void main(String[] args) {
 		WebDriver hospiol = new ChromeDriver();
 		hospiol.manage().window().maximize();
-		
-		
+		hospiol.get("https://testing.hospiol.com/site/login");
+		hospiol.findElement(By.name("username")).sendKeys("sampath.marolix@gmail.com");
+		hospiol.findElement(By.name("password")).sendKeys("sampath@777");
+		hospiol.findElement(By.xpath("//button[@type='submit']")).click();
 	}
 	
 
