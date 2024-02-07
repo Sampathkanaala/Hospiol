1da96b6bd885541555cf3e06cf9e4113b291b91d
admin <admin@DESKTOP-03U0ACR>
2024-02-07 18:32:58 +0530

    I have created the project.

--- src/test/java/com/login/Login.java
+++ src/test/java/com/login/Login.java
@@ -1,7 +1,18 @@
 package com.login;
 
-public class Login {
-	
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login{
+	public static void main(String[] args) {
+		WebDriver hospiol = new ChromeDriver();
+		hospiol.manage().window().maximize();
+		hospiol.get("https://testing.hospiol.com/site/login");
+		hospiol.findElement(By.name("username")).sendKeys("sampath.marolix@gmail.com");
+		hospiol.findElement(By.name("password")).sendKeys("sampath@777");
+		hospiol.findElement(By.xpath("//button[@type='submit']")).click();
+	}
 	
 
 }
